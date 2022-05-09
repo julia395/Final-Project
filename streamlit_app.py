b67061d25f7b9e9f26db828030d99c730a9c6fec
root <root@p-3c5109e0-d4f5-47cd-9c2e-1a9d1a674c6f.projects-domain.projects.svc.cluster.local>
2022-05-09 05:49:44 +0000

    REPLACE THIS WITH YOUR COMMIT MESSAGE

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,180 +1,75 @@
-
-##### This app is just an extremely simple example.
-##### See the Streamlit documentation for how to create more complex apps.
-
 import pandas as pd
 import numpy as np
-
-business = pd.read_csv('business.csv')
-
-#removing businesses that are permenatley closed 
-business = business[business['is_open'] == 1]
-
-#splitting the string of categories for each row into a list 
-categories = business['categories'].str.split(", ")
-
-#replacing the categories column with a list for each row 
-business['categories'] = categories
-
-#remove missing values in the categories column 
-categories = categories.dropna()
-
-#creating a list of all the categories by iterating through the nested list 
-#of categories and flattening the list 
-flatlist = []
-for sublist in categories:
-    for element in sublist:
-        flatlist.append(element)
-
-#removing duplicates from the flattened list of categories 
-category_list = []
-for category in flatlist:
-    if category not in category_list:
-       category_list.append(category)
-
-#changing flattened list of all categories into pandas series and counting occurance 
-#of each category then creating a dictionary
-categories_series = pd.Series(flatlist)
-occurances = categories_series.value_counts()
-occurances_dict = occurances.to_dict()
-
-# keeping only one category for each business based on which category has the 
-# most businesses
-
-#add column of number of occurances
-business_exploded = business.explode('categories')
-occur = business_exploded['categories'].map(occurances_dict)
-business_exploded['occurance_of_category'] = occur
-
-#sort rows by business id then occurance of category so the same business is kept 
-#together and the most popular category for the business will be listed first
-business = business_exploded.sort_values(by=['business_id','occurance_of_category'], ascending=False)
-
-#keeping only one row for each business that has the most popular category
-business = business.drop_duplicates(subset='business_id', keep='first')
-
-# keeping only businesses that in top 1 percentile of businesses with most of reviews within each category 
-rank = business.groupby('categories')['review_count'].rank(pct=True)
-business['percent_rank'] = rank
-top_reviewed_bus = business[business['percent_rank'] >= 0.99]
-
-import requests 
-
-bus_id_list = top_reviewed_bus['business_id'].tolist()
-
-review_df = pd.DataFrame()
-api_key = 'ghT50Lq_KgKyg-kKuwT5Hvv-gAAGprAWRdHyo50TPgeHlrWSG54jvmo9CGGRjlKwfm2uq5x4e8i7i_JOd49-DulkbwDZRTsve8chhPe59MtuiEM9qfXpHhwpq5RpYnYx'
-headers = {'Authorization': "Bearer {}".format(api_key)}
-for bus_id in bus_id_list:
-   req = requests.get('https://api.yelp.com/v3/businesses/{}/reviews'.format(bus_id), headers = headers)
-   data = req.json()
-   if 'error' not in data:
-    df_response = pd.DataFrame(data['reviews'])
-    df_response['business_id'] = bus_id
-    review_df = review_df.append(df_response)
-
-df_review_bus = pd.merge(top_reviewed_bus, review_df , on='business_id')
-df_review_bus = df_review_bus.drop(columns=['address','is_open'])
-
-#should i save as file here so data is static?
-df_review_bus.to_csv('df_review_bus.csv')
-
-## RUN FROM HERE 
 import streamlit as st
-import pandas as pd
+import requests 
+import matplotlib.pyplot as plt
 
-#load csv file i just created
-df_yelp_data = pd.read_csv('df_review_bus.csv')
-
-#change time_created to date
-df_yelp_data['time_created']= pd.to_datetime(df_yelp_data['time_created'])
-
-#go back and change for streamlit
-#category_chosen = st.sidebar.selectbox("Select a Category", df_yelp_data.categories.unique())
-category_chosen = 'Restaurants'
-category_filter = df_yelp_data[df_yelp_data['categories'] == category_chosen]
-
-subset = df_yelp_data[df_yelp_data['categories'] == category_chosen]
-aft_mar_2022 = subset[subset['time_created'] >= 'March 01, 2022 00:00']
-bef_mar_2022 = subset[subset['time_created'] < 'March 01, 2022 00:00']
-
-txt_aft_2022 = ' '.join(aft_mar_2022['text']).lower()
-txt_bef_2022 = ' '.join(bef_mar_2022['text']).lower()
-
-from nltk.tokenize import word_tokenize
-from nltk import FreqDist
-from nltk.corpus import stopwords
-
+import nltk
 nltk.download('punkt')
+from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk import FreqDist
 
-word_tokens_before = word_tokenize(txt_bef_2022)
-word_tokens_after = word_tokenize(txt_aft_2022)
+#add title to page
+st.title('Yelp Business Data')
+
+#reading updated business and review csv
+business = pd.read_csv('bussiness_updated.csv')
+reviews = pd.read_csv('df_review_bus.csv')
+
+#displaying datframe of business data
+st.dataframe(data=business, width=None, height=None)
+
+#have user select category
+with st.sidebar:
+    st.subheader("User Inputs for Graph")
+    category_chosen = st.sidebar.selectbox("Select a Category", reviews.categories.unique())
+
+#subset of yelp data based on the category chosen 
+category_subset = reviews[reviews['categories'] == category_chosen]
+
+#taking all reviews from dataframe, joining them into one string, and making all the
+#characters lowercase 
+reviews_txt = ' '.join(category_subset['text']).lower()
+
+#splitting into tokens
+word_tokens = word_tokenize(reviews_txt)
+
+#making list of stop words
 stop_words = set(stopwords.words('english'))
 
-before_words = []
-for word in word_tokens_before:
+#filterng out stop words and none words
+words = []
+for word in word_tokens:
     if word.isalpha() and word not in stop_words:
-        before_words.append(word)
+        words.append(word)
 
-amount_of_words_b = len(before_words)
+#getting frequency distrabution
+frequency = FreqDist(words)
 
-after_words = []
-for word in word_tokens_after:
-    if word.isalpha() and word not in stop_words:
-        after_words.append(word)
+#creating dataframes
+dist = pd.DataFrame(frequency.items(),columns=['word', 'freq'])
 
-amount_of_words_a = len(after_words)
+#sorting by frequency 
+dist = dist.sort_values(by='freq', ascending=False).reset_index(drop=True)
 
-before_frequency = FreqDist(before_words)
-after_frequency = FreqDist(after_words)
+#selecting only top 10
+dist = dist.iloc[:10, :]
 
-before_dist = pd.DataFrame(before_frequency.most_common(20),columns=['Word', 'Frequency'])
-after_dist = pd.DataFrame(after_frequency.most_common(20),columns=['Word', 'Frequency'])
+#getting data for graph 
+words = dist['word']
+frequency = dist['freq']
 
-b_percentage = []
-b_frequency = before_dist["Frequency"].tolist()
+#create figure
+fig = plt.figure()
+#title for graph 
+title = f"10 Most Frequent Words in Yelp Reviews for {category_chosen} category of Businesses"
+#setting up graph and labels
+plt.barh(words, frequency)
+plt.ylabel('Word')
+plt.xlabel('Word Frequency')
+plt.title(title)
+st.pyplot(fig)
 
-for freq in b_frequency:
-    percent = freq/amount_of_words_b
-    formated = "{:.2%}".format(percent)
-    b_percentage.append(formated)
-
-before_dist['Percent Frequency'] = b_percentage
-
-a_percentage = []
-a_frequency = after_dist["Frequency"].tolist()
-
-for freq in a_frequency:
-    percent = freq/amount_of_words_a
-    formated = "{:.2%}".format(percent)
-    a_percentage.append(formated)
-
-after_dist['Percent Frequency'] = a_percentage
-
-st.write(after_dist)
-st.write(before_dist)
-
-##### Title and intro
-
-#st.title( 'Example Streamlit App' )
-#st.write( '''
-#This app is very small and does almost nothing.
-#It's just an example.
-#''' )
-
-
-##### Inputs
-
-#st.header( 'Choose two numbers' )
-#a = st.slider( label='a', min_value=1, max_value=10, value=2, step=1 )
-#b = st.slider( label='b', min_value=1, max_value=10, value=3, step=1 )
-
-
-##### Output
-
-#st.header( 'Tiny computations')
-#st.write( f'{a} + {b} = {a+b}' )
-#st.write( f'{a} - {b} = {a-b}' )
-#st.write( f'{a} * {b} = {a*b}' )
-#st.write( f'{a} / {b} = {a/b}' )
+st.markdown("*Only includes reviews for reviews from the top 1% of businesses based on review volume within each business category.")
